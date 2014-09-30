31814601760d4c05401807283f2f344a66051d6c
joyrexus <joyrexus@gmail.com>
2014-09-29 21:58:42 -0500

    fix spacing

--- test.js
+++ test.js
@@ -4,11 +4,14 @@ var Parser = require('./');
 var sheet = new Parser('sample.xlsx', 'Transcript');
 
 test('values', function(t) {
+
     t.plan(1);
     t.same(sheet.values('XYZ'), [ 'x', 'y', 'z', 'q', 'b' ]);
 });
 
+
 test.skip('records', function(t) {
+
     t.plan(1);
     t.equal(sheet.records.length, 5);
 });
